e5a8056ed96f042a9429042d6933e53f3fde3fb3
sollse <solk1202@gmail.com>
2023-03-15 11:59:29 +0900

    book info/review page done

--- static/functions.js
+++ static/functions.js
@@ -3,12 +3,6 @@ function newShelfForm() {
     document.getElementById("popupBox").style.display = 'block';
 };
 
-// delete bookshelf
-function delete_bookshelf(){
-  console.log("hello");
-
-};
-
 // Hide pop up box when "delete" button is clicked
 function closePopup(){
     document.getElementById("popupBox").style.display = 'none';
